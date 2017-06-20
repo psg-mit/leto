ec2b3a87ade0f2e06b5b475140ba7dd7a1b75eda
Brett Boston <boston@csail.mit.edu>
2017-06-20 17:45:40 -0400

    Upgraded Z3 to 4.5.0
    Also removed unncessary sudos
    
    Closes #20

--- setup.bash
+++ setup.bash
@@ -7,17 +7,17 @@ apt-get update
 apt-get install -y bisonc++ flexc++ cpp make gcc g++ python python3
 ) &
 (
-wget "https://github.com/Z3Prover/z3/archive/z3-4.4.1.tar.gz"
-tar xvf z3-4.4.1.tar.gz
+wget "https://github.com/Z3Prover/z3/archive/z3-4.5.0.tar.gz"
+tar xvf z3-4.5.0.tar.gz
 ) &
 wait
 
-cd z3-z3-4.4.1
+cd z3-z3-4.5.0
 python scripts/mk_make.py
 cd build
 make
-sudo make install
-sudo apt-get install -y clang
+make install
+apt-get install -y clang
 cd /vagrant/src
 make clean
 make
