94b016db2e30d998a36753224e354d258d3d0ce4
Brett Boston <boston@csail.mit.edu>
2018-04-11 11:58:23 -0400

    Modified seu.c to be close to jacobi.c

--- programs/vecprod/seu.c
+++ programs/vecprod/seu.c
@@ -1,18 +1,22 @@
-property_r bounded_error(matrix<real> v, uint i) :
-  exists(uint ei)
-    (forall(uint fi)((fi < i<r> && fi != ei) -> v<o>[fi] == v<r>[fi]));
+#define EPSILON real(1000, 1)
 
-uint N;
-matrix<int> x(N);
-matrix<int> y(N);
-matrix<int> result(N);
+property_r bounded_diff(matrix<real> x, uint N) :
+  forall(uint fi)((fi < N<r>) -> (-EPSILON < x<o>[fi] - x<r>[fi] < EPSILON));
 
-@label(loop)
-for (uint i = 0; i < N; ++i)
-    (1 == 1)
-    ((model.upset == false -> eq(result)) &&
-     (model.upset == true -> bounded_error(result, i))) {
-  result[i] = x[i] *. y[i];
+requires forall(uint fi)(0 <= x[fi] < 100 && 0 <= y[fi] < 100)
+r_requires eq(N) && eq(x) && eq(y)
+matrix<real> product(uint N, matrix<real> x(N), matrix<real> y(N)) {
+
+  matrix<real> result(N);
+
+  @label(loop)
+  for (uint i = 0; i < N; ++i)
+      (1 == 1)
+      (bounded_diff(result, N)) {
+    result[i] = x[i] *. y[i];
+  }
+
+  relational_assert(bounded_diff(result, N));
+
+  return result;
 }
-
-relational_assert(bounded_error(result, N));
