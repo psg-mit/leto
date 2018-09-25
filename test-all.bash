e39312e33a309df6dd01c70a170926aa4ecaabfb
Brett Boston <boston@csail.mit.edu>
2018-09-24 23:12:05 -0400

    Documentation

--- test-all.bash
+++ test-all.bash
@@ -10,11 +10,12 @@ function run-test {
   ./pp.py $1 $2 | /usr/bin/time -v src/leto
 }
 
-run-test models/pseudo-seu-range.c programs/vecprod/seu.c "Vector product (Figure 4) under addtive SEU (Figure 3)"
-run-test models/switchable-rowhammer.c programs/vecprod/rowhammer.c "Vector product (Figure 6) under switchable rowhammer (Figure 5)"
-run-test models/multicycle-add.c programs/vecprod/seu.c "Vector product (Figure 4) under multicycle (Figure 7)"
-run-test models/multicycle-refined.c programs/vecprod/seu.c "Vector product (Figure 4) under refined SEU (Figure 8)"
-run-test models/pseudo-seu-range-mult.c programs/jacobi/jacobi-mult-weak.c "Jacobi (Figure 14) under multiplicative SEU (Figure 13)"
-run-test models/pseudo-seu-range.c programs/conj-grad/conj-grad.c "SS-CG (Appendix H) under additive SEU (Figure 3)"
-run-test models/pseudo-seu-chaos.c programs/sd/sd-freq-errs.c "SS-SD (Figure 31) under unbounded SEU (Figure 1)"
-run-test models/switchable-rowhammer.c programs/cc/switchable-cc.c "SC-CC (Figure 27) under switchable rowhammer (Figure 5)"
+run-test models/pseudo-seu-chaos.c programs/vecprod/seu-dmr.c "Vector product (Figure 3) under SEU (Figure 1)"
+run-test models/pseudo-seu-range-mult.c programs/vecprod/seu-mult.c "Vector product (Figure 7) with relative error (Figure 6)"
+run-test models/pseudo-seu-range-mult.c programs/jacobi/jacobi-mult-weak.c "Jacobi under multiplicative SEU"
+run-test models/pseudo-seu-range.c programs/conj-grad/conj-grad.c "SS-CG under additive SEU"
+run-test models/pseudo-seu-chaos.c programs/sd/sd-freq-errs.c "SS-SD under unbounded SEU"
+run-test models/pseudo-seu-range.c programs/sd/sd-freq-errs.c "SS-SD under additive SEU"
+run-test models/pseudo-seu-range-mult.c programs/sd/sd-freq-errs.c "SS-SD under multiplicative SEU"
+run-test models/switchable-rowhammer.c programs/cc/switchable-cc.c "SC-CC under switchable rowhammer"
+run-test models/switchable-multicycle-rowhammer.c programs/cc/switchable-cc.c "SC-CC under multicycle"
